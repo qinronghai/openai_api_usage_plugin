1abdc680a1655937b40ff4cc1d2b332d586d6c09
SeaEliy <185043843@qq.com>
2023-04-30 15:31:46 +0800

    refactor: 💄 重构响应式布局，添加宽度过渡

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        "625px": "625px",
+        "785px": "785px",
+        "885px": "885px",
+      },
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1480px",
+      "2xl": "1920px",
+    },
   },
   plugins: [],
 };
